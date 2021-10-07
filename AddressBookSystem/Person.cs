a04eb336d02243439f4211c214ee94400768567c
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-07 20:26:37 +0530

    [Hanif]:Added a program to edit a particular contact detail in the given address book

--- AddressBookSystem/Person.cs
+++ AddressBookSystem/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    // UC-2: Ability to add a new contact in the address book
+    // UC-3: To edit a particular contact from the given list of contact details from the given address book
     class Person
     {
         string firstName, lastName, city, state, address, email;
@@ -58,6 +58,85 @@ namespace AddressBookSystem
         public static void AddNewContact(List<Person> personList)
         {
             personList.Add(new Person("ABC", "XYZ", "Bengaluru", "Karnataka", "Hoodi","abc@xyz.com", 444555, 9998887771));
+        }
+
+        // Editing a particular contact detail from the current address book
+        public static void SearchName(List<Person> personList, string FName)
+        {
+            int index = -1;
+            for (int i = 0; i < personList.Count; i++)
+            {
+                if (personList[i].firstName == FName)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("{0} is present in the list", FName);
+                    Console.WriteLine();
+                    index = i;
+
+                    // Providing an option to the user as to which detail about a particluar person needs to be changed
+
+                    Console.WriteLine("Enter any one of the following options given below:");
+                    Console.WriteLine("Enter 1 to change to change the first name");
+                    Console.WriteLine("Enter 2 to change to change the last name");
+                    Console.WriteLine("Enter 3 to change to change the city name");
+                    Console.WriteLine("Enter 4 to change to change the state name");
+                    Console.WriteLine("Enter 5 to change to change the address");
+                    Console.WriteLine("Enter 6 to change to change the email ID");
+                    Console.WriteLine("Enter 7 to change to pincode");
+                    Console.WriteLine("Enter 8 to change the phone number");
+                    Console.WriteLine();
+                    Console.Write("Enter your choice:");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("Enter a new first name: ");
+                            personList[index].firstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.Write("Enter a new last name: ");
+                            personList[index].lastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.Write("Enter a new city name: ");
+                            personList[index].city = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.Write("Enter a new state name: ");
+                            personList[index].state = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.Write("Enter a new address: ");
+                            personList[index].address = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.Write("Enter a new email ID: ");
+                            personList[index].email = Console.ReadLine();
+                            break;
+                        case 7:
+                            Console.Write("Enter a new pincode: ");
+                            personList[index].pinCode = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 8:
+                            Console.Write("Enter a new phone number: ");
+                            personList[index].phoneNum = Convert.ToInt64(Console.ReadLine());
+                            break;
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("The new list after editing is: ");
+                    DisplayContactDetails(personList);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("{0} is not present in the list", FName);
+                    Console.WriteLine();
+                    Console.WriteLine("The original list is:");
+                    DisplayContactDetails(personList);
+                }
+            }
         }
         
             // Displaying person details
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -15,6 +15,10 @@ namespace AddressBookSystem
             Person.CreatePersonDetails(personList); // Method calling to create a person's contact list
             Person.AddNewContact(personList); // Method calling to enter a new contact in the address book
             Person.DisplayContactDetails(personList); // Method calling to display the address book contact details
+            Console.WriteLine();
+            Console.Write("Enter the first name of the person in the current list which you want to modify: ");
+            string FName = Console.ReadLine();
+            Person.SearchName(personList, FName); // Method calling to search a person name in the list and edit the contact details accordingly
             Console.ReadLine();
         }
     }
